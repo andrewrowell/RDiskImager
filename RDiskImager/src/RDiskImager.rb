04a37271a0471320c7933da8e1d7765aa2361e32
andrewjrowell <admin@andrewrowell.info>
2014-08-21 13:49:12 -0500

    separated code that gets input from the user from code that runs the commands

--- RDiskImager/src/RDiskImager.rb
+++ RDiskImager/src/RDiskImager.rb
@@ -42,37 +42,37 @@ class RDiskImager
     puts "Device to Image [1]\nImage to Device [2]\n"
     choice = gets().chomp
     if choice == "1"
-      deviceToImage()
+      deviceToImageDialog()
     end
     if choice == "2"
-      imageToDevice()
+      imageToDeviceDialog()
     end
   end
   
-  # Write a device to an image
-  def deviceToImage()
+  # Prompts user for the device and image names
+  def deviceToImageDialog()
     puts "Device name? (sda, hdb, sdc, etc...)"
     device_name = gets.chomp
-    
+        
     # read the device's size from /proc/partitions
     devtable = `cat /proc/partitions | grep "#{device_name}"`
     # split("\n")[0] to get the first line (information
     #   for the entire device) and then split(" ")[2] to
     #   get the right number from that line
     size = devtable.split("\n")[0].split(" ")[2]
-    
+        
     puts "Image name?"
     image_name = gets.chomp
     puts "Are you sure you want to save an image of #{device_name} to #{image_name}? y/[n]"
     if gets.chomp == "y"
-      system "dd if=\"/dev/#{device_name}\"| pv -s #{size}k | dd of=\"#{image_name}\""
+      deviceToImage(device_name, size, image_name)
     else
       puts "Cancelling operation!"
     end
   end
   
-  # Write an image to a device
-  def imageToDevice()
+  # Prompts user for image and device names
+  def imageToDeviceDialog()
     puts "Image name?"
     image_name = gets.chomp
     size = File.size(image_name)/1024
@@ -81,10 +81,20 @@ class RDiskImager
     device_name = gets.chomp
     puts "Are you sure you want to write #{image_name} to #{device_name}? y/[n]"
     if gets.chomp == "y"
-      system "dd if=\"#{image_name}\"| pv -s #{size}k | dd of=\"/dev/#{device_name}\""
+      imageToDevice(image_name, size, device_name)
     else
       puts "Cancelling operation!"
     end
+  end
+  
+  # Write a device to an image
+  def deviceToImage(device_name, size, image_name)
+    system "dd if=\"/dev/#{device_name}\"| pv -s #{size}k | dd of=\"#{image_name}\""
+  end
+  
+  # Write an image to a device
+  def imageToDevice(image_name, size, device_name)
+    system "dd if=\"#{image_name}\"| pv -s #{size}k | dd of=\"/dev/#{device_name}\""
   end
   
 end
